a75c0f73bd463d012496260e5e5dae891527cee2
钟海 <417233956@qq.com>
2020-04-22 13:22:54 +0800

    spring boot init

---
+++ src/main/java/com/mango/log2mongodb/Log2mongodbApplication.java
@@ -0,0 +1,13 @@
+package com.mango.log2mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Log2mongodbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Log2mongodbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mango/log2mongodb/Log2mongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mango.log2mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Log2mongodbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
